a055d30ace2b71495deddc774999d49a00e74a54
Jaydin <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2022-12-27 13:27:13 -0500

    Added Function
    Added Login Function to login to BlackBoard

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,6 @@
 import requests
 
+
 # Ping URL function
 def ping_url(url):
     try:
@@ -7,5 +8,26 @@ def ping_url(url):
         return r.status_code
     except Exception as e:
         return e
+
+
+# Login function
+def login(session, url, username, password):
+    # set up the payload
+    payload = {'user_id': username, 'password': password}
+
+    # Make the request
+    r = session.post(url + 'webapps/login/', data=payload)
+
+    # Check if login was successful
+    if r.url == url + '/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1':
+        print('Login successful!')
+        return True
+    elif r.url == url + 'webapps/login/':
+        print('Login failed!')
+        return False
+    else:
+        print('An error occurred!')
+        return False
+
 
 
