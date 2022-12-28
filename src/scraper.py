e36a51e9adc0a9db026871dfbbb00b5dab7cdaae
Jaydin <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2022-12-28 02:14:09 -0500

    Reduction
    Too many function in the initial approach. Each function wasn't tested so new approach to recreate each function one at a time.

--- src/scraper.py
+++ src/scraper.py
@@ -1,144 +1,39 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
 
-# Ping URL function
-def ping_url(url):
-    try:
-        r = requests.get(url)
-        return r.status_code
-    except Exception as e:
-        return e
-
-
-# Login function
-def login(session, url, username, password):
-    # set up the payload
-    payload = {'user_id': username, 'password': password}
-
-    # Make the request
-    r = session.post(url + 'webapps/login/', data=payload)
-
-    # Check if login was successful
-    if r.url == url + '/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1':
-        print('Login successful!')
-        return True
-    elif r.url == url + 'webapps/login/':
-        print('Login failed!')
-        return False
-    else:
-        print('An error occurred!')
-        return False
-
-# Get the course IDs of the courses the user is enrolled in
-
-
-def get_course_content_links(session, url, course_id, content_id):
-    # Set the URL of the course content page
-    content_url = url + '/webapps/blackboard/content/listContent.jsp?course_id=' + \
-        course_id + '&content_id=' + content_id
-
-    # Make a GET request to the course content page
-    response = session.get(content_url)
-
-    # Parse the HTML content
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Find all the links to the content files
-    links = soup.find_all('a', {'class': 'item'})
-
-    # Create a list to store the URLs of the content files
-    file_urls = []
-
-    # Loop through the links
-    for link in links:
-        # Get the href attribute of the link
-        href = link.get('href')
-
-        # If the href attribute contains the word "listContent" it is a link to a subfolder
-        if 'listContent' in href:
-            # Get the URL of the subfolder
-            url = href.split('?', 1)[0]
-            # Get the ID of the subfolder
-            id = href.split('?', 1)[1].split('=')[1]
-
-            # Get the file URLs in the subfolder
-            subfolder_urls = get_course_content_links(
-                session, url, course_id, id)
-
-            # Add the file URLs in the subfolder to the list of file URLs
-            file_urls.extend(subfolder_urls)
-
-        # If the href attribute contains the word "content" it is a link to a file
-        elif 'content' in href:
-            # Get the URL of the file
-            url = href.split('?', 1)[0]
-            # Get the ID of the file
-            id = href.split('?', 1)[1].split('=')[1]
-
-            # Get the download URL of the file
-            file_url = get_file_download_url(session, url, course_id, id)
-
-            # Add the download URL to the list of file URLs
-            file_urls.append(file_url)
-
-    return file_urls
-
-
-def get_file_download_url(session, url, course_id, content_id):
-    # Set the URL of the file download page
-    download_url = url + '/webapps/blackboard/content/launchLink.jsp?course_id=' + \
-        course_id + '&content_id=' + content_id
-
-    # Make a GET request to the file download page
-    response = session.get(download_url)
-
-    # Parse the HTML content
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Find the link to the file
-    link = soup.find('a', {'class': 'resourceUrl'})
-
-    # Get the href attribute of the link
-    href = link.get('href')
-
-    return href
-
-# Get the download URL of a file
-
-
-def download_file(session, url, file_url):
-    file_response = session.get(file_url)
-    if file_response.status_code == 200:
-        return file_response.content
-    else:
-        raise Exception("An error occurred while downloading the file")
-
-
-def download_course_content(username, password):
-    # Set the URL you want to scrape
-    url = 'https://blackboard.kettering.edu/'
-
-    # Create a session to persist cookies
+# Username: Free8864
+# Password: P@ssword00!?!?!
+def login_and_get_courses(username, password):
+    # Send a POST request to the login form with the username and password
+    login_url = "https://blackboard.kettering.edu/"
+    login_data = {"username": username, "password": password}
     session = requests.Session()
+    response = session.post(login_url, data=login_data)
 
-    # Attempt to login
-    if login(session, url, username, password):
-        # Get the course IDs and content IDs of the courses the user is enrolled in
-        courses = get_course_ids(session, url)
+    # Check if the login was successful
+    if response.status_code != 200:
+        raise Exception("Login failed")
 
-        # Download the course content for each course
-        for course in courses:
-            course_id = course['course_id']
-            content_id = course['content_id']
-            print(f'Downloading course content for course {course_id}...')
-            links = get_course_content_links(
-                session, url, course_id, content_id)
-            for link in links:
-                file_url = link['file_url']
-                file_name = link['file_name']
-                download_file(session, url, file_url, file_name)
-                print(f'  Downloaded {file_name}')
-        print('Download complete!')
-    else:
-        print('Login failed!')
+    print("Login successful")
+
+    # Get the HTML source code of the page after logging in
+    page_url = "https://kettering.blackboard.com/webapps/portal/execute/tabs/tabAction?tabId=_1_1&tab_tab_group_id=_1_1"
+    page_response = session.get(page_url)
+    page_html = page_response.text
+    print("Got page HTML")
+
+    # Parse the HTML to extract the list of courses
+    soup = BeautifulSoup(page_html, "html.parser")
+    # TODO Fix this
+    courses_element = soup.find("div", id="_4_1termCourses__208_1")
+    course_elements = courses_element.find_all("li", class_="course")
+    courses = []
+    for course_element in course_elements:
+        course_name = course_element.find("h3").text
+        courses.append({"name": course_name})
+
+    return courses
+
+# Call the login function
+print(login_and_get_courses("Free8864", "P@ssword00!?!?!"))
