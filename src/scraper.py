438ed91a5276314d7d74e1535be4c7197b0f7008
Jaydin <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2022-12-28 14:07:22 -0500

    Update's
    Got passed the login screen, along with clicking the accept cookies button now attempting to get courses

--- src/scraper.py
+++ src/scraper.py
@@ -1,38 +1,66 @@
-# Import the necessary modules
-from     import webdriver
-from selenium.webdriver.common.by import By
+def scrappy(username, password):
+    # Import the necessary modules
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
 
-# Create a webdriver object and set the desired options
-driver = webdriver.Chrome(executable_path="/path/to/chromedriver")
-driver.implicitly_wait(10) # Wait for up to 10 seconds for elements to become available
+    # Create a webdriver object and set the desired options
+    driver = webdriver.Chrome(executable_path="/path/to/chromedriver")
+    # Wait for up to 10 seconds for elements to become available
+    driver.implicitly_wait(10)
 
-# Go to the login page
-driver.get("https://blackboard.kettering.edu/")
+    # Go to the login page
+    driver.get("https://blackboard.kettering.edu/")
 
-# Wait for the redirect to occur
-while driver.current_url == "https://blackboard.kettering.edu/":
-    pass
+    # Wait for the redirect to occur
+    while driver.current_url == "https://blackboard.kettering.edu/":
+        pass
 
-# The redirect has occurred, so we can now login
+    # The redirect has occurred, so we can now login
 
-# Find the username field 
-login_form = driver.find_element(By.ID, "loginForm")
+    # Find the username field
+    login_form = driver.find_element(By.ID, "loginForm")
 
-# Find the username and password fields
-username_field = login_form.find_element(By.ID, "inputUserID")
+    # Find the username and password fields
+    username_field = login_form.find_element(By.ID, "inputUserID")
 
-password_field = login_form.find_element(By.ID, "inputPassword")
+    password_field = login_form.find_element(By.ID, "inputPassword")
 
-# Enter the username and password
+    # Enter the username and password
 
-username = "Free8864"
-password = "P@ssword00!?!?!"
+    username_field.send_keys(username)
+    password_field.send_keys(password)
 
-username_field.send_keys(username)
-password_field.send_keys(password)
+    # Find the login button and click it
+    login_button = login_form.find_element(By.ID, "sign-button")
+    # Click the login button
+    login_button.click()
+
+    # Wait for the redirect to occur
+
+    while driver.current_url == "https://blackboard.kettering.edu/webapps/login/":
+        pass
+
+    # On the redirected page accept the cookies policy
+    cookies_button = driver.find_element(By.ID, "agree_button")
+    cookies_button.click()
+    
+    # Find the list element that contains the class information
+    class_list = driver.find_element_by_css_selector(".portletList-img.courseListing.coursefakeclass.u_indent")
+
+    # Find the list items within the list element
+    class_items = class_list.find_elements(By.TAG_NAME, "li")
+
+    # Extract the text of the 'a' element within each list item
+    classes = []
+    for item in class_items:
+        class_link = item.find_element(By.TAG_NAME, "a")
+        class_text = class_link.text
+        classes.append(class_text)
+
+    print(classes)
 
 
-print("Logging in...")
+scrappy("Free8864", "7G5@Tbr$w#amSgT")
 
-
-
+# Username = "Free8864"
+# password = "7G5@Tbr$w#amSgT"
