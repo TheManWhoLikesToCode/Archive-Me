707536491408b321fecceb44f3309a0202753957
Jaydin <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2022-12-28 13:31:15 -0500

    Switch scrappers
    Switched from soup to selenium. I did this because I was having trouble debugging soup.

--- src/scraper.py
+++ src/scraper.py
@@ -1,39 +1,38 @@
-from bs4 import BeautifulSoup
-import requests
+# Import the necessary modules
+from     import webdriver
+from selenium.webdriver.common.by import By
+
+# Create a webdriver object and set the desired options
+driver = webdriver.Chrome(executable_path="/path/to/chromedriver")
+driver.implicitly_wait(10) # Wait for up to 10 seconds for elements to become available
+
+# Go to the login page
+driver.get("https://blackboard.kettering.edu/")
+
+# Wait for the redirect to occur
+while driver.current_url == "https://blackboard.kettering.edu/":
+    pass
+
+# The redirect has occurred, so we can now login
+
+# Find the username field 
+login_form = driver.find_element(By.ID, "loginForm")
+
+# Find the username and password fields
+username_field = login_form.find_element(By.ID, "inputUserID")
+
+password_field = login_form.find_element(By.ID, "inputPassword")
+
+# Enter the username and password
+
+username = "Free8864"
+password = "P@ssword00!?!?!"
+
+username_field.send_keys(username)
+password_field.send_keys(password)
 
 
-# Username: Free8864
-# Password: P@ssword00!?!?!
-def login_and_get_courses(username, password):
-    # Send a POST request to the login form with the username and password
-    login_url = "https://blackboard.kettering.edu/"
-    login_data = {"username": username, "password": password}
-    session = requests.Session()
-    response = session.post(login_url, data=login_data)
+print("Logging in...")
 
-    # Check if the login was successful
-    if response.status_code != 200:
-        raise Exception("Login failed")
 
-    print("Login successful")
 
-    # Get the HTML source code of the page after logging in
-    page_url = "https://kettering.blackboard.com/webapps/portal/execute/tabs/tabAction?tabId=_1_1&tab_tab_group_id=_1_1"
-    page_response = session.get(page_url)
-    page_html = page_response.text
-    print("Got page HTML")
-
-    # Parse the HTML to extract the list of courses
-    soup = BeautifulSoup(page_html, "html.parser")
-    # TODO Fix this
-    courses_element = soup.find("div", id="_4_1termCourses__208_1")
-    course_elements = courses_element.find_all("li", class_="course")
-    courses = []
-    for course_element in course_elements:
-        course_name = course_element.find("h3").text
-        courses.append({"name": course_name})
-
-    return courses
-
-# Call the login function
-print(login_and_get_courses("Free8864", "P@ssword00!?!?!"))
